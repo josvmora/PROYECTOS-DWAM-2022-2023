7b890b18bac6f7c92a239f5e323c1e31f071ef90
Jose Andres Vivanco Mora <69166962+josvmora@users.noreply.github.com>
2022-11-29 17:39:33 -0500

    importando libreria chart css

--- Proyecto02/js/script.js
+++ Proyecto02/js/script.js
@@ -5,8 +5,50 @@ window.onload = function () {
   
   document.getElementById('cargar').addEventListener('click', () => {
 
-    let array = ["116778","90125","70345"]
-    for(x of array){
+    let array_1 = ["116778","90125","70345"]
+    let array_2 = ["1224","37707","1706"]
+
+    function peticion_1(arreglo){
+
+      for(x of arreglo){
+
+        let id = x
+        //let url = `https://api.jikan.moe/v4/anime/${id}/full`
+        let url = `https://api.jikan.moe/v4/manga/${id}/full`
+    
+        fetch(url)
+          .then(response => response.json())
+          .then(obj => {
+    
+            // Inicio del procesamiento
+            
+            let data = obj['data']
+    
+            document.getElementById('respuesta').innerHTML += `
+              <div class="card">
+                
+                <div class="card-body">
+                  <h5 class="card-title">${data.title}</h5>
+                  <p>${data.scored_by}</p>
+                  
+                </div>
+              </div>
+            `
+            // Fin del procesamiento
+            
+          })
+          .catch(error => {
+            document.getElementById('respuesta').innerHTML = JSON.stringify(error.message)
+          })
+        }
+
+    }
+
+    peticion_1(array_2);
+    setTimeout(peticion_1, 3000, array_1);
+
+
+    /*for(x of array){
 
     let id = x
     //let url = `https://api.jikan.moe/v4/anime/${id}/full`
@@ -37,6 +79,7 @@ window.onload = function () {
         document.getElementById('respuesta').innerHTML = JSON.stringify(error.message)
       })
     }
+    */
   })
 
 }
